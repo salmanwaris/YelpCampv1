dca3157e6fff5e9d78ee2a01a743e3dec0c05fe7
Salman Waris Khan <65291339+salmanwaris@users.noreply.github.com>
2021-06-14 12:33:40 +0530

    upto images

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
 const campground = require('./models/campground');
 const { urlencoded } = require('express');
@@ -20,6 +21,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate)
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
 
