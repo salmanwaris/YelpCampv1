3b1ed6a874447ee551d13944c3f43f3378a606b2
Salman Waris Khan <65291339+salmanwaris@users.noreply.github.com>
2021-06-16 16:31:25 +0530

    edit

--- models/campground.js
+++ models/campground.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Review = require('./review'); 
+const Review = require('./review');
 
 const CampgroundSchema = new Schema({
     title: String,
