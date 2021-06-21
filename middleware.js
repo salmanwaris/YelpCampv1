9f36ff4a344a101e09b0392b7807af8f7f1fb13d
Salman Waris Khan <65291339+salmanwaris@users.noreply.github.com>
2021-06-21 19:58:32 +0530

    authorization completed with help of bill

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
-const Review = require('./models/review')
+const Review = require('./models/review');
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
--- routes/reviews.js
+++ routes/reviews.js
@@ -4,9 +4,9 @@ const Campground = require('../models/campground');
 const Review = require('../models/review');
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
-const { validateReview, isLoggedIn,isReviewAuthor} = require('../middleware');
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 
-router.post('/', validateReview, isLoggedIn, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     review.author = req.user._id;
