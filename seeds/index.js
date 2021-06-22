518b657ddf4e3a80d9eb031fe11e8b1e6ed1f19a
Salman Waris Khan <65291339+salmanwaris@users.noreply.github.com>
2021-06-22 22:36:19 +0530

    image upload part 2

--- seeds/index.js
+++ seeds/index.js
@@ -24,7 +24,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '60d09c5fa952c60493607924',
+            author: '60d212d36af1ad051fe294fd',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
