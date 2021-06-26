d53fa4176ca3948b67b49c8b3e6c92899df820fd
Salman Waris Khan <65291339+salmanwaris@users.noreply.github.com>
2021-06-26 12:25:51 +0530

    final IA

--- seeds/index.js
+++ seeds/index.js
@@ -24,7 +24,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '60d6c0794393e800155cbfbe',
+            author: '60d6c00b4393e800155cbfbd',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
